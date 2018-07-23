36a82055886caae5eb8fa4304f3383089d82b88b
Kriscotiano <christian.o-10@hotmail.com>
2018-07-22 19:30:36 -0500

    Added final JavaScript questions

--- resources/js/app.js
+++ resources/js/app.js
@@ -54,6 +54,7 @@ function populate() {
     }
 };
 
+//Selection of a choice
 function guess(id, guess) {
     let button = document.getElementById(id);
     button.onclick = function() {
@@ -62,12 +63,14 @@ function guess(id, guess) {
     }
 };
 
+//Footer progress
 function showProgress() {
     let currentQuestionNum = quiz.questionIndex + 1;
     let element = document.getElementById('progress');
     element.innerHTML = "Question " + currentQuestionNum + " of " + quiz.questions.length;
 };
 
+//End game results
 function showScores() {
     let gameOverHtml = "<h1>Result</h1>";
     gameOverHtml += "<h2>" + quiz.score + " correct out of " + quiz.questions.length + "</h2>";
@@ -78,13 +81,16 @@ function showScores() {
 //Questions
 let questions = [
     new Question('Which of the following is NOT a primitive type in JavaScript?', ['Boolean', 'Undefined', 'Object', 'String'], 'Object'),
-    new Question('What will 3 > 2 > 1 return?', ['3', 'True', 'Undefined', 'False'], 'False'),
-    new Question('What is the result? Number("1") - 1 == 0;', ['True', 'False', 'TypeError', 'Null'], 'True'),
+    new Question('Which built in method combines the text of two strings and returns a new string?', ['append()', 'attach()', 'concat()', 'None of the above'], 'concat()'),
+    new Question('What will 3 > 2 > 1 return?', ['3', 'True', 'Undefined', 'False'], 'False'),    
     new Question('What is the function of the Array object that adds and/or removes elements from an array?', ['splice()', 'slice()', 'unshift()', 'filter()'], 'splice()'),
-    new Question('Which of the following function of Array object adds one or more elements to the end of an array and returns the new length of the array?', ['pop()', 'join()', 'push()', 'shift()'], 'push()'),
-    new Question('When does the keyword "this" reference anything in the code?', ['As soon as it\'s written', 'When an event listener is added', 'When the browser is loaded', 'When the function/method containing it is invoked'], 'When the function/method containing it is invoked')
+    new Question('Which of the following methods of Array object adds one or more elements to the end of an array and returns the new length of the array?', ['pop()', 'join()', 'push()', 'shift()'], 'push()'),
+    new Question('When does the keyword "this" reference anything in the code?', ['As soon as it\'s written', 'When an event listener is added', 'When the browser is loaded', 'When the function/method containing it is invoked'], 'When the function/method containing it is invoked'),
+    new Question('Which of these is a feature of AJAX?', ['Send and retreive server data asynchronously', 'Live form validations', 'Change content without reloading the page', 'All of the above'], 'All of the above'),
+    new Question('Every object is linked to a _______ object from which it can inherit properties.', ['Prototype', 'Parameter', 'Class', 'Constructor'], 'Prototype'),
+    new Question('Where do you use the “break” statement?', ['To divide a mathematical value', 'To terminate an object statement', 'To terminate a switch statement, loop or block', 'To divide an array at a specified index'], 'To terminate a switch statement, loop or labeled block'),
+    new Question('Immutability, avoidance of shared state & side effects are all examples of _______.', ['Functional Programming', 'Const', 'Object Methods', 'All of the above'], 'Functional Programming')
 ];
-
 let quiz = new Quiz(questions);
 
 
